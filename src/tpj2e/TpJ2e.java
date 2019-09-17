55c1652e26083002c6bfd21c92e6531a56620f1a
Bryan <pedago@pcalb-mm0603.univ-jfc.fr>
2019-09-17 16:03:32 +0200

    Hello World

---
+++ src/tpj2e/TpJ2e.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpj2e;
+
+/**
+ *
+ * @author pedago
+ */
+public class TpJ2e {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World");
+    }
+    
+}
